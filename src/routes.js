aa69cd666c4e2ff79c0556c9b7c602435727e262
Sherlyne <sherlynea1994@hotmail.com>
2024-08-19 17:28:58 +0300

    modified route file

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,32 @@
-
+import { Actors } from "react-dom/test-utils";
+import ErrorPage from "./pages/ErrorPage"; 
 
 const routes = [
-   
-  ];
+  {
+     path: "/", 
+     element:<Home/>
+     errorElement: 
+  }, 
+   {
+     path: "/actors", 
+     element:<Actors/>
+     errorElement: 
+  }, 
+   {
+     path: "/directors", 
+     element:<Directors/>
+     errorElement: 
+  }, 
+   {
+     path: "/movies", 
+     element:<Movies/>
+     errorElement: 
+  }, 
+   {
+     path: "/", 
+     element:<Home/>
+     errorElement: 
+  }, 
+   ];
 
 export default routes;
